2520e892dc47d4488dccc6c821445ac9f83db6cf
remo5000 <vigneshshankar@gmail.com>
2018-02-15 16:20:24 +0800

    add package.json and wip index.js

---
+++ Storage/index.js
@@ -0,0 +1,24 @@
+console.log("index.js executing");
+var provider = new Web3.providers.HttpProvider("http://localhost:8545");
+var contract = TruffleContract({
+  abi: ...,
+  unlinked_binary: ...,
+  address: ..., // optional
+  // many more
+});
+console.log(contract);
+
+var MyContract = contract()
+
+MyContract.setProvider(provider);
+
+var deployed;
+
+MyContract.deployed().then(function(instance) {
+      var deployed = instance;
+      return instance.someFunction(5);
+}).then(function(result) {
+      // Do something with the result or continue with more transactions.
+});
+
+
